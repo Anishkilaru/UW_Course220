6c7f38c158ba39055fe29f5817e8546c24e20729
AnishKilaru <kilaruanishsai@gmail.com>
2020-06-03 12:40:52 -0700

    Latest-Check_In

--- ContactSolution/ContactApp/MainWindow.xaml.cs
+++ ContactSolution/ContactApp/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ContactApp.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,11 +25,80 @@ namespace ContactApp
         public MainWindow()
         {
             InitializeComponent();
+            LoadContacts();
         }
+
+        private void LoadContacts()
+        {
+            var contacts = App.ContactRepository.GetAll();
+
+            uxContactList.ItemsSource = contacts
+                .Select(repositoryContact => ContactModel.ToModel(repositoryContact))
+                .ToList();
+
+            // OR
+            //var uiContactModelList = new List<ContactModel>();
+            //foreach (var repositoryContactModel in contacts)
+            //{
+            //    This is the .Select(t => ... )
+            //    var uiContactModel = ContactModel.ToModel(repositoryContactModel);
+            //
+            //    uiContactModelList.Add(uiContactModel);
+            //}
+            //
+            //uxContactList.ItemsSource = uiContactModelList;
+        }
+
+        private GridViewColumnHeader listViewSortCol = null;
+        private SortAdorner listViewSortAdorner = null;
+
+        private void GridViewColumnHeader_Click(object sender, RoutedEventArgs e)
+        {
+            var column = (sender as GridViewColumnHeader);
+
+            string sortBy = column.Tag.ToString();
+            if (listViewSortCol != null)
+            {
+                AdornerLayer.GetAdornerLayer(listViewSortCol).Remove(listViewSortAdorner);
+                uxContactList.Items.SortDescriptions.Clear();
+            }
+
+            ListSortDirection newDir = ListSortDirection.Ascending;
+            if (listViewSortCol == column && listViewSortAdorner.Direction == newDir)
+                newDir = ListSortDirection.Descending;
+
+            listViewSortCol = column;
+            listViewSortAdorner = new SortAdorner(listViewSortCol, newDir);
+            AdornerLayer.GetAdornerLayer(listViewSortCol).Add(listViewSortAdorner);
+            uxContactList.Items.SortDescriptions.Add(new SortDescription(sortBy, newDir));
+        }
+
 
         private void uxFileNew_Click(object sender, RoutedEventArgs e)
         {
+            var window = new ContactWindow();
 
+            if (window.ShowDialog() == true)
+            {
+                var uiContactModel = window.Contact;
+
+                var repositoryContactModel = uiContactModel.ToRepositoryModel();
+
+                App.ContactRepository.Add(repositoryContactModel);
+
+                LoadContacts();
+
+                // OR
+                //App.ContactRepository.Add(window.Contact.ToRepositoryModel());
+            }
+        }
+
+        private void uxFileChange_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void uxFileDelete_Click(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
